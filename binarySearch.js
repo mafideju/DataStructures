665ee9623e54cb09b8ea1fd3623b57e3e6edcc31
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-10-29 23:20:40 -0300

    Binary Search 1

---
+++ binarySearch.js
@@ -0,0 +1,12 @@
+const factorial = num => {
+  if (num === 1) {
+    return num
+  } else {
+    return num * factorial(num - 1)
+  }
+}
+const fact = factorial(4);
+
+setTimeout(() => {
+  console.log('total:', fact)
+}, 1800);
